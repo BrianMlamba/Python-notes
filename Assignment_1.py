8b91a3984c17b09be939797f245fed23811a8219
Brian Mlamba <smbsolomon@gmail.com>
2026-04-14 14:29:07 +0300

    updated the Assignment

--- Assignment_1.py
+++ Assignment_1.py
@@ -2,6 +2,10 @@ print(2+2) #addition
 print(5-2) # subtraction
 print(5*5) # multiplication
 print(25/5)# divison
+import math # square root of a number 
+num = 121
+result = math.sqrt(num)
+print(result)
 import random # random number btw 1 and 100
-random.randint(1,100)
-
+number = random.randint(1, 100)
+print (number)
