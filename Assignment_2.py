d6c04e2621f0a210676596582a8ac4861e5cda68
Brian Mlamba <smbsolomon@gmail.com>
2026-04-15 10:14:55 +0300

    added assignment_2

---
+++ Assignment_2.py
@@ -0,0 +1,10 @@
+name = input('what is your name /n ') # Question 1
+print(name)
+input('welcome Brian')
+Hours = input('How many hours did you work /n ') # Question 2
+print(Hours)
+Gross_pay = int (Hours) * 2.75 
+print(Gross_pay)
+celcius = float(input("Enter temperature in Celsius: ")) # Question 3
+Fahreinheit = (celcius * 9/5) + 32
+print(Fahreinheit)
